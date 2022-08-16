1d0ed1d9eeefe539122fbc0d39a3b5f251607b0d
tanyongzhao <tanyongzhao@tuzhanai.com>
2022-08-16 17:33:37 +0800

    sort: 希尔排序

--- sort/shell.ts
+++ sort/shell.ts
@@ -6,28 +6,20 @@ function shellSort(arr: number[]) {
         arr[x] = swapNum
     }
 
-    // 增量（排序步长）
-    let incr = Math.floor(arr.length / 2)
-
     // 增量循环
-    while (incr) {
+    for (let incr = Math.floor(arr.length / 2); incr; incr = Math.floor(incr / 2)) {
         // 每组需要排序的数组
-        for (let startIndex = 0; startIndex < incr; startIndex++) {
-            // 内部使用冒泡排序
-            let lastIndex = Math.floor(arr.length / incr) * incr + startIndex
-            while (lastIndex > 1) {
-                for (let i = 0; i <= lastIndex; i = incr + i) {
-                    if (arr[i] > arr[i + incr]) {
-                        swap(arr, i, i + incr)
-                    }
+        for (let lastIndex = incr; lastIndex < arr.length; lastIndex++) {
+            for (let i = lastIndex; i > 0; i = i - incr) {
+                if (arr[i] < arr[i - incr]) {
+                    swap(arr, i, i - incr)
+                } else {
+                    break
                 }
-                lastIndex = lastIndex - incr
             }
         }
 
-        incr = Math.floor(incr / 2)
     }
-
     return arr
 }
 
